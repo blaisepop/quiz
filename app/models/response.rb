fe08019bac242b6efed6a61a0a2401e0af1fbfac
Blaise <blaisepopote@gmail.com>
2019-02-17 16:30:46 +0100

    remove require

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,7 +4,6 @@ class Response < ApplicationRecord
 	belongs_to :question
 
 	def good?
-		require 'fuzzystringmatch'
 		jarow = FuzzyStringMatch::JaroWinkler.create( :native )
 		distance =  jarow.getDistance(  self.content, self.question.valid_response )
 		return distance > 0.8
