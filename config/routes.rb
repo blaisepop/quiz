6aa65b55137cf32e1a416100926c8e972e3d7989
Blaise <blaisepopote@gmail.com>
2019-02-20 19:36:30 +0100

    changes authlogic

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :responses
   resources :questions
   get 'signup',to:'users#new'
+  resources :user_sessions ,only:[:new,:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
