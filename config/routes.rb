a0b880caf04559bc3d773848894849668f15e7d0
Blaise <blaisepopote@gmail.com>
2019-02-20 19:02:49 +0100

    add columns user db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :responses
   resources :questions
+  get 'signup',to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
