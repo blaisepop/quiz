1fd0491bb107db4bae61d70728bc2e37ac2907a4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-03-25 13:25:22 +0100

    Correct Authlogic integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ApplicationRecord
-    acts_as_authentic
-    has_many :responses
-    has_many :questions
+  attr_accessor :password, :password_confirmation
+  acts_as_authentic
+
+  has_many :responses
+  has_many :questions
+
   validates :email, uniqueness: {
-    message: "déjà existant" }
+    message: 'déjà existant'
+  }
 
   validates :email,
-    format: {
-      with: /.+@.+\.[a-z]+/,
-      message: "doit être une adresse mail"
-    },
-    length: { maximum: 100 }
-
+            format: {
+              with: /.+@.+\.[a-z]+/,
+              message: 'doit être une adresse mail'
+            },
+            length: { maximum: 100 }
 end
