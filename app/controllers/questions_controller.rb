b8bd73781dbf71ff2a2b1458ddd2e0f33da098d2
Blaise <blaisepopote@gmail.com>
2019-04-04 19:05:23 +0200

    add field in view and params in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -70,6 +70,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title,:valid_response)
+      params.require(:question).permit(:title,:valid_response,:tags)
     end
 end
