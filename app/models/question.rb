ec975ed308f405e6bb8f2404047e7b803939801b
Blaise <blaisepopote@gmail.com>
2019-04-09 19:36:40 +0200

    add method to show tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,14 +3,17 @@ class Question < ApplicationRecord
 	validates_presence_of :valid_response
 	validates :valid_response, length: {minimum:2}
 
-	
+
 	# we create a method that can be called anywhere
 	# Loop over each questions in databases and get all existings tags
 	# @return [Array] someting like `['electro', 'music']`
 	def self.all_existing_tags
-		return []
-	end
-
-	def save
+		Question.all
+						.pluck(:tags)
+						.map { |tags| tags.split(',') }
+						.flatten
+						.uniq
+						.compact
+						.sort
 	end
 end
