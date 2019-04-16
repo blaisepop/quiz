d51c4e3fc6fd24794042c9d0f8d934046f7e9bf9
Blaise <blaisepopote@gmail.com>
2019-04-16 19:57:33 +0200

    correct user issue with nil

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :modify, Question, user_id: user.id
-    can :modify, Question if user.admin?
-    can :modify, Question if user.super_admin?
-    can :modify, Response, user_id: user.id
-    can :modify, Response if user.admin?
-    can :modify, Response if user.super_admin?
-
+    unless user.nil?
+      can :modify, Question, user_id: user.id
+      can :modify, Question if user.admin?
+      can :modify, Question if user.super_admin?
+      can :modify, Response, user_id: user.id
+      can :modify, Response if user.admin?
+      can :modify, Response if user.super_admin?
+    end
   end
 end
