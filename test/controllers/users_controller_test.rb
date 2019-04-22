d2c58ef4b724fcff2d6a853b2d0955c3e8c40c79
Blaise <blaisepopote@gmail.com>
2019-04-22 15:31:46 +0200

    little corrections in view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,20 +43,34 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    login @user
     get edit_user_url(@user)
     assert_response :success
   end
 
+  test "should forbid edit" do
+    get edit_user_url(@user)
+    assert_response :forbidden
+  end
+
   test "should update user" do
+    login @user
     patch user_url(@user), params: { user: { email: @user.email } }
     assert_redirected_to user_url(@user)
   end
 
   test "should destroy user" do
+    login @user
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
 
     assert_redirected_to users_url
+  end
+
+  test "should forbid destroy user" do
+    assert_no_difference('User.count') do
+      delete user_url(@user)
+    end
   end
 end
